a0c371f7e1c5c0aa7ca87a8a948d5c307c7dd126
Tim Rasim <neogucky@gmail.com>
2019-09-29 17:31:41 +0200

    development commit

--- plugin.rb
+++ plugin.rb
@@ -1,20 +1,22 @@
-# name: discourse-onesignal
-# about: Push notifications via the OneSignal API.
-# version: 1.0
-# authors: pmusaraj
-# url: https://github.com/pmusaraj/discourse-onesignal
+# name: discourse-aws
+# about: Push notifications via AWS SNS
+# version: 0.1
+# authors: pmusaraj, neogucky
+# originally developed by pmusaraj
 
 after_initialize do
-  if SiteSetting.onesignal_push_enabled
+  if SiteSetting.aws_push_enabled
     ONESIGNALAPI = 'https://onesignal.com/api/v1/notifications'
+
+   Rails.logger.info("after init aws")
 
     DiscourseEvent.on(:post_notification_alert) do |user, payload|
 
-      if SiteSetting.onesignal_app_id.nil? || SiteSetting.onesignal_app_id.empty?
+      if SiteSetting.aws_app_id.nil? || SiteSetting.aws_app_id.empty?
           Rails.logger.warn('OneSignal App ID is missing')
           return
       end
-      if SiteSetting.onesignal_rest_api_key.nil? || SiteSetting.onesignal_rest_api_key.empty?
+      if SiteSetting.aws_rest_api_key.nil? || SiteSetting.aws_rest_api_key.empty?
           Rails.logger.warn('OneSignal REST API Key is missing')
           return
       end
@@ -25,18 +27,19 @@ after_initialize do
           .pluck(:client_id, :push_url)
 
       if clients.length > 0
-        Jobs.enqueue(:onesignal_pushnotification, clients: clients, payload: payload, username: user.username)
+        Jobs.enqueue(:aws_pushnotification, clients: clients, payload: payload, username: user.username)
       end
 
     end
 
     module ::Jobs
-      class OnesignalPushnotification < Jobs::Base
+      class AwsPushnotification < Jobs::Base
         def execute(args)
           payload = args["payload"]
+          Rails.logger.info("maybe aws key?  #{args['clients']}.")
 
           params = {
-            "app_id" => SiteSetting.onesignal_app_id,
+            "app_id" => SiteSetting.aws_app_id,
             "contents" => {"en" => "#{payload[:username]}: #{payload[:excerpt]}"},
             "headings" => {"en" => payload[:topic_title]},
             "data" => {"discourse_url" => payload[:post_url]},
@@ -53,7 +56,7 @@ after_initialize do
 
           request = Net::HTTP::Post.new(uri.path,
               'Content-Type'  => 'application/json;charset=utf-8',
-              'Authorization' => "Basic #{SiteSetting.onesignal_rest_api_key}")
+              'Authorization' => "Basic #{SiteSetting.aws_rest_api_key}")
           request.body = params.as_json.to_json
           response = http.request(request)
 
@@ -66,10 +69,8 @@ after_initialize do
             Rails.logger.error("#{response.to_yaml}")
 
           end
-
         end
       end
     end
-
   end
 end
